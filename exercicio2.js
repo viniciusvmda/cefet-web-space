93af463571ee612a421f1a2179b6735e2bd8d669
Vinícius <viniciusvmda@gmail.com>
2018-04-19 08:34:19 -0300

    Comecei parte 2

--- exercicio2.js
+++ exercicio2.js
@@ -1,11 +1,17 @@
-// Faça o exercício da galeria de imagens aqui
-// Este arquivo ainda não está incluído no arquivo HTML
+const servidorDasImagens = {
+	url: 'https://fegemo.github.io/cefet-web/images/',
+  	todasAsImagens: [
+	    'philae-parts.jpg',
+	    'philae-rosetta.jpg',
+	    'philae-separation.jpg',
+	    'philae-67-picture.jpg',
+	    'philae-collecting.jpg'
+  	]
+};
 
-const servidorDasImagens = 'https://fegemo.github.io/cefet-web/images/',
-  todasAsImagens = [
-    'philae-parts.jpg',
-    'philae-rosetta.jpg',
-    'philae-separation.jpg',
-    'philae-67-picture.jpg',
-    'philae-collecting.jpg'
-  ];
+// adiciona imagens ao html
+let galeria = document.querySelector('#galeria');
+for (imagem of servidorDasImagens.todasAsImagens) {
+	let imagemEl = document.createElement('img');
+	imagemEl.src = servidorDasImagens.url + imagem
+}
